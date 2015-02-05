24407e392f2d6ae92852886eb24b7d85a14ab798
mjbp <mickperkins@gmail.com>
2015-02-05 12:11:45 +0000

    Hover effect added, Demo added, updated gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var pkg = require('./package.json');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var header = require('gulp-header');
+var deploy = require('gulp-gh-pages');
+
+var banner = [
+    '/**',
+    ' * @name <%= pkg.name %>: <%= pkg.description %>',
+    ' * @version <%= pkg.version %> <%= new Date().toUTCString() %>',
+    ' * @author <%= pkg.author %>',
+    ' * @license <%= pkg.license %>',
+    ' * @url <%= pkg.repository.url %>',
+    ' */'
+].join('\n');
+
+var dest = 'dist/';
+var demo = 'demo/';
+
+gulp.task('compile', function() {
+    return gulp.src('BBCCarousel.js', {cwd: 'src'})
+        .pipe(header(banner + '\n', { pkg: pkg }))
+        .pipe(gulp.dest(dest));
+});
+
+gulp.task('compress', function() {
+    return gulp.src(dest + '<%= pkg.name %>.js')
+        .pipe(uglify({preserveComments: 'some'}))
+        .pipe(rename('BBCCarousel.min.js'))
+        .pipe(gulp.dest(dest));
+});
+
+gulp.task('copy', function(){
+    return gulp.src(dest + '/**/*')
+        .pipe(gulp.dest(demo));
+});
+
+gulp.task('deploy', function () {
+    return gulp.src('./demo/**/*')
+        .pipe(deploy());
+});
+
+gulp.task('watch', function() {
+    gulp.watch(['src/*.js', 'demo/**/*'], ['compile']);
+});
